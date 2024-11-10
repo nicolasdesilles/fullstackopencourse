2b3a0dee80f5523e6fdd941d7f46ccc41dd3cfc9
Nicolas Désilles <nicolas@ndlabs.fr>
2024-11-10 17:43:40 +0100

    Finished Exercise 2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+
+const Person = ({person}) => {
+
+  return (
+    <div>
+      {person.name}
+    </div>
+  )
+
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNewNameChange = (event) => {
+    // console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNewNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {
+          persons.map(person =>
+            <Person key={person.name} person={person}/>
+          )
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App
